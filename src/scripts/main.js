689f773d881101a178e0014d2ed8257c75eb71a2
Mykhailo Firka <ltcar@online.ua>
2025-01-01 16:12:57 +0200

    task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+
+function parseSalary(salaryString) {
+  return Number(salaryString.replace(/[^0-9.-]+/g, ''));
+}
+
+function sortListSalary(list) {
+  const sortedEmployees = Array.from(list.children).sort((a, b) => {
+    return parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary);
+  });
+
+  sortedEmployees.forEach((employee) => list.appendChild(employee));
+}
+
+function getEmployees(list) {
+  return Array.from(list.children).map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: parseSalary(item.dataset.salary),
+    age: Number(item.dataset.age),
+  }));
+}
+
+sortListSalary(employeeList);
+
+const employeeArray = getEmployees(employeeList);
+
+employeeArray.innerHTML = '';
+
+sortListSalary.forEach((item) => {
+  employeeList.appendChild(item);
+});
